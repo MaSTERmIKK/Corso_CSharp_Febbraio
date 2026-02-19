fb4698bb59ff9b19fb9d113205397fcb6d3c6cba
Edoardo Del Sarto <e99delsarto@gmail.com>
2026-02-19 14:27:09 +0100

    Aggiungti esempi Metodi

---
+++ Giornate corso/04_Giovedì_19_02/Metodi/Program.cs
@@ -0,0 +1,50 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine($"Il risultato della somma tra 10 e 30 è {Somma(10,30)}");
+        Console.WriteLine($"Il risultato della somma tra 15 e 180 è {Somma(15,180)}");
+        Console.WriteLine($"Il risultato della somma tra 150 e 1286 è {Somma(11505,11286)}");
+
+        int somma = 0;
+        somma = Somma(12, 89);
+        Console.WriteLine($"\nLa somma tra 12 e 89 è {somma}");
+
+        int numero1 = 178;
+        int numero2 = 123;
+        somma = Somma(numero1, numero2);
+        Console.WriteLine($"\nLa somma tra {numero1} e {numero2} è {somma}");
+
+        // Saluta("Edoardo");
+
+        ConfrontaNumero();
+    }
+
+    //=============== METODO CON TIPO DI RITORNO ===============
+    public static int Somma(int a, int b)
+    {
+        int risultato = a + b;
+        return risultato;
+    }
+
+    //=============== METODO VOID ===============
+    public static void Saluta(string nome)
+    {
+        Console.WriteLine($"Buongiorno {nome}, come va?");
+    }
+
+    //=============== SCOPE ===============
+    static int x = 5;
+    public static void ConfrontaNumero()
+    {
+        int y = 10;
+        if(x > y)
+        {
+            Console.WriteLine($"{x} è maggiore di {y}");
+        }
+        else
+        {
+            Console.WriteLine($"{x} è minore di {y}");
+        }
+    }
+}

86bb0cc61634e3841323892cc2ce5f4be5514070
Edoardo Del Sarto <e99delsarto@gmail.com>
2026-02-20 17:26:07 +0100

    Esempi metodi ref, out, overload

--- Giornate corso/04_Giovedì_19_02/Metodi/Program.cs
+++ Giornate corso/04_Giovedì_19_02/Metodi/Program.cs
@@ -2,27 +2,31 @@
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine($"Il risultato della somma tra 10 e 30 è {Somma(10,30)}");
-        Console.WriteLine($"Il risultato della somma tra 15 e 180 è {Somma(15,180)}");
-        Console.WriteLine($"Il risultato della somma tra 11505 e 11286 è {Somma(11505,11286)}");
+        // Console.WriteLine($"Il risultato della somma tra 10 e 30 è {Somma(10,30)}");
+        // Console.WriteLine($"Il risultato della somma tra 15 e 180 è {Somma(15,180)}");
+        // Console.WriteLine($"Il risultato della somma tra 11505 e 11286 è {Somma(11505,11286)}");
 
-        int somma = 0;
-        somma = Somma(12, 89);
-        Console.WriteLine($"\nLa somma tra 12 e 89 è {somma}");
+        // int somma = 0;
+        // somma = Somma(12, 89);
+        // Console.WriteLine($"\nLa somma tra 12 e 89 è {somma}");
 
         int numero1 = 178;
         int numero2 = 123;
+        int somma  = 0;
         somma = Somma(numero1, numero2);
         Console.WriteLine($"\nLa somma tra {numero1} e {numero2} è {somma}");
 
-        Saluta("Edoardo");
 
-        ConfrontaNumero();
+        // Saluta("Edoardo");
+
+        // ConfrontaNumero();
     }
 
     //=============== METODO CON TIPO DI RITORNO ===============
     public static int Somma(int a, int b)
     {
+        a = 500;
+        Console.WriteLine($"Valore di a = {a}");
         int risultato = a + b;
         return risultato;
     }
---
+++ Giornate corso/05_Venerdì_20_02/MetodiParte2/Program.cs
@@ -0,0 +1,109 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        //=============== OVERLOAD =====================
+        /* Stampa("Ciao");
+        // Stampa(10);
+        // Stampa(10, "Ciao");
+        // Console.WriteLine($"il nuovo numero è {Stampa(10f)}");
+
+        Console.WriteLine($"SommaInt = {Somma(10, 20)}");
+        Console.WriteLine($"SommaFloat = {Somma(10.4f, 20.3f)}");*/
+        
+        //================ REF =========================
+        /*int n = 0;
+        if(IncrementaEControlla(ref n))
+        {
+            Console.WriteLine($"pari");
+        }
+        else
+        {
+            Console.WriteLine($"dispari");
+        }
+
+        Console.WriteLine($"N = {n}");*/
+    
+        //================ OUT =========================
+        /* int numero;
+        // Calcolo(out numero);
+        // Console.WriteLine($"Numero = {numero}");
+
+        // float perimetroTriangolo;
+        // float areaTriangolo;
+        // DescriviTriangoloEquilatero(3, 5, out perimetroTriangolo, out areaTriangolo);
+        // Console.WriteLine($"Il triangolo di l 3 e a 5 ha perimetro {perimetroTriangolo} e area {areaTriangolo}");
+
+        // string nStringa= "26";
+        // int strConvertita;
+        // if(int.TryParse(nStringa, out strConvertita))
+        // {
+        //     Console.WriteLine($"Conversione Riuscita con successo. {nStringa} è {strConvertita}");
+        // }
+        // else
+        // {
+        //     Console.WriteLine($"Conversione Fallita");
+        // }*/
+    }
+
+#region Overload Methods
+    static void Stampa(int numero)
+    {
+        Console.WriteLine($"il numero è {numero}");
+    }
+
+    static void Stampa(string messaggio)
+    {
+        Console.WriteLine($"il messaggio è {messaggio}");
+    }
+
+    static void Stampa(int numero, string messaggio)
+    {
+        Console.WriteLine($"il numero è {numero} e il messaggio è {messaggio}");
+    }
+
+    static float Stampa(float numero)
+    {
+        Console.WriteLine($"il numero è {numero}");
+        return numero + 10;
+    }
+
+
+    static int Somma(int a, int b)
+    {
+        return a + b;
+    }
+
+    static float Somma(float a, float b)
+    {
+        return a + b;
+    }
+
+#endregion
+
+#region REF Methods
+    public static bool IncrementaEControlla(ref int numero)
+    {
+        numero++;
+        return numero % 2 == 0;
+    }
+
+    public static int Incrementa(int numero)
+    {
+        return numero++;
+    }
+#endregion
+
+#region OUT Methods
+    private static void Calcolo(out int risultato)
+    {
+        risultato = 10;
+    }
+
+    private static void DescriviTriangoloEquilatero(float l, float a, out float perimetro, out float area)
+    {
+        perimetro = l*3;
+        area = l*a/2;
+    }
+#endregion
+}

0d76373f34a98a11329506a40e1038a7cf1d89d5
Edoardo Del Sarto <e99delsarto@gmail.com>
2026-02-26 14:16:24 +0100

    Aggiunti esempi dizionario Aggiunti Esempi Enum

---
+++ Giornate corso/08_Giovedì_26_02/Enum/Classe.cs
@@ -0,0 +1,30 @@
+﻿using Semaforo = Programm.Semaforo;
+class Classe
+{
+    private Semaforo semaforo;
+    public static void Main(string[] args)
+    {
+        // Console.WriteLine($"Prodotto 1: {Prodotto.Gelato}");
+
+        Prodotto prod = Prodotto.Gelato;
+
+        // switch (prod)
+        // {
+        //     case Prodotto.Gelato:
+        //         break;
+        //     case Prodotto.Mele:
+        //         break;
+        //     case Prodotto.Olio:
+        //         break;
+        //     default:
+        //         break;
+        // }
+
+        List<Semaforo> semafori = new List<Semaforo>();
+
+        semafori.Add(Semaforo.Rosso);
+        semafori.Add(Semaforo.Rosso);
+        semafori.Add(Semaforo.Verde);
+    }
+}
+
---
+++ Giornate corso/08_Giovedì_26_02/Enum/Program.cs
@@ -0,0 +1,41 @@
+﻿public enum Prodotto
+{
+    Gelato, //0
+    Olio,  // 1
+    Mele   //2
+}
+
+class Programm
+{
+    public enum Semaforo
+    {
+        Rosso,
+        Giallo,
+        Verde
+    }
+    public static void Main(string[] args)
+    {
+        // Console.WriteLine($"Prodotto 1: {Prodotto.Gelato}");
+
+        // Prodotto prod = Prodotto.Gelato;
+
+        // switch (prod)
+        // {
+        //     case Prodotto.Gelato:
+        //         break;
+        //     case Prodotto.Mele:
+        //         break;
+        //     case Prodotto.Olio:
+        //         break;
+        //     default:
+        //         break;
+        // }
+
+        List<Semaforo> semafori = new List<Semaforo>();
+
+        semafori.Add(Semaforo.Rosso);
+        semafori.Add(Semaforo.Rosso);
+        semafori.Add(Semaforo.Verde);
+    }
+}
+
